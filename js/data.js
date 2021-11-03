93f8dceeb77b0f250d80ea633524611674b23833
Rory Deken <rory.deken@th2.com>
2021-11-03 12:56:06 -0400

    Rory picks chargers

--- js/data.js
+++ js/data.js
@@ -6,7 +6,7 @@ var managers = [
 	{owner: 'Luke Huffer', team: 'DAL'},
 	{owner: 'Aidan Guckenberger', team: 'BAL'},
 	{owner: 'KC Johnson', team: 'CIN'},
-	{owner: 'Rory Deken', team: ''},
+	{owner: 'Rory Deken', team: 'LAC'},
 	{owner: 'Jake Bedinghaus', team: ''},
 	{owner: 'Ricky Brayton', team: ''}
 ];
